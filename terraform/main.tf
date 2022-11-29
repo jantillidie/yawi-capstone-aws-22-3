d779cb792137ecd32e5d064d47edac3cf8fe2895
Jan Maaß <git@antipost.de>
2022-11-29 16:35:53 +0100

    Update tfstate for sandbox without restrictions

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,11 +4,11 @@ terraform {
   # to switch from local to remote backend:
 
   backend "s3" {
-    bucket = "yawi-tfstate-bucket-aws223"
+    bucket = "yawi-tfstate-bucket-aws223-758393"
     key    = "terraform.tfstate"
     region = "us-west-2"
-    # dynamodb_table = "terraform-state-locking"
-    # encrypt        = true
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
   }
 
   required_providers {
@@ -28,33 +28,33 @@ provider "aws" {
 #   instance_type = "t2.micro"
 # }
 
-# resource "aws_s3_bucket" "terraform_state" {
-#   bucket        = "yawi-tfstate-bucket-aws223-758393"
-#   force_destroy = true
-# }
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "yawi-tfstate-bucket-aws223-758393"
+  force_destroy = true
+}
 
-# resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
-#   bucket = aws_s3_bucket.terraform_state.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
-# resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
-#   bucket = aws_s3_bucket.terraform_state.bucket
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm = "AES256"
-#     }
-#   }
-# }
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
-# resource "aws_dynamodb_table" "terraform_locks" {
-#   name         = "terraform-state-locking"
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "LockID"
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# }
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
